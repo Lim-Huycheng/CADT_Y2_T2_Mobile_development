9b314a7c017ed924b5750f1d725d18713229abb4
KCLP1995 <chhaylymea43154001@gmail.com>
2025-03-21 10:58:51 +0700

    A

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:human_firewall/Lymean/welcome.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white, // Sets the background color to white
+        primaryColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0, // Removes shadow
+          iconTheme: IconThemeData(color: Colors.black), // Ensures icons are visible
+        ),
+        colorScheme: ColorScheme.light(
+          primary: Colors.white, 
+          secondary: Colors.blue, // Adjust as needed
+        ),
+      ),
+      home: const Welcome(),
+    );
+  }
+}
