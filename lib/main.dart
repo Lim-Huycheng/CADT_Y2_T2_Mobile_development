be10363d6fd12efac29bb2cfb7710fd93cedc584
KCLP1995 <chhaylymea43154001@gmail.com>
2025-03-30 10:32:42 +0700

    A

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:human_firewall/Lymean/welcome.dart';
-import 'package:human_firewall/Huycheng/Homepage.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  
   await Firebase.initializeApp(
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
           secondary: Colors.blue, // Adjust as needed
         ),
       ),
-      home: const HomeScreen(),
+      home: const Welcome(),
     );
   }
 }
