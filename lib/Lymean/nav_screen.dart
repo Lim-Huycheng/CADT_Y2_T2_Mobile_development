b558301c60fb3a1f8d119e0a2f2efd99fdf0de85
KCLP1995 <chhaylymea43154001@gmail.com>
2025-03-31 14:07:57 +0700

    A

---
+++ lib/Lymean/nav_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:human_firewall/Huycheng/HomePage0.dart';
+import 'package:human_firewall/Chornay/Community_newfeed.dart'; // Main community feed page
+import 'package:human_firewall/Kanya/quizz_category.dart';
+
+class NavScreen extends StatefulWidget {
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  int pageIndex = 0; // Default index for Home screen
+
+  List<Widget> pageList = <Widget>[
+    HomeScreen(),           // Home screen
+    PostFeedPage(),         // Main community feed page
+    QuizHome(),             // Quiz home page
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pageList[pageIndex],  // Display the corresponding page based on pageIndex
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 70,
+        backgroundColor: Colors.white,
+        color: Color(0xFF0081D7),
+        animationDuration: Duration(milliseconds: 300),
+        index: pageIndex,  // Sync the navigation bar index with pageIndex
+        onTap: (index) {
+          setState(() {
+            pageIndex = index;  // Update the pageIndex when tapping nav bar items
+          });
+        },
+        items: [
+          Icon(Icons.home, color: Colors.white, size: 35),  // Home icon
+          Icon(Icons.people, color: Colors.white, size: 35),  // Community icon
+          Icon(Icons.quiz_outlined, color: Colors.white, size: 35),  // Quiz icon
+        ],
+      ),
+    );
+  }
+}
