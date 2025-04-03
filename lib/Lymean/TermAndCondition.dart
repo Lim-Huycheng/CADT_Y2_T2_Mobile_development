45a442fe3efe3a6472958bcf853679a4f7941aa6
KCLP1995 <chhaylymea43154001@gmail.com>
2025-04-03 08:45:03 +0700

    A

--- lib/Lymean/TermAndCondition.dart
+++ lib/Lymean/TermAndCondition.dart
@@ -41,15 +41,35 @@ class _TermAndConditionsState extends State<TermAndConditions> {  // Fixed the s
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 80,
-        title: Text(
-          "Terms and Conditions",
-          style: TextStyle(
-            fontSize: 30, // Set font size to 30
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xFF0081D7),
+        title: null,
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.only(top: 25.0),
+          child: Center(
+            child: const Text(
+              'Terms & condition',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'SourceSerif',
+                color: Colors.white,
+                letterSpacing: 1.2,
+              ),
+            ),
           ),
         ),
-        backgroundColor: Color.fromARGB(255, 2, 105, 173),
-        centerTitle: true, // Center the title
+        elevation: 4,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        ),
+        toolbarHeight: 80,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back when pressed
+          },
+        ),
       ),
       body: SingleChildScrollView(  // To allow scrolling if the content overflows
         child: Padding(
